a7aef6a345257f84f3b95bd3fc0d1ecb4ff650f4
unknown <mindsdet292@iiht.tech>
2022-05-23 13:05:11 +0530

    Anusha

---
+++ Comprehensive/src/test/java/Automation/MindtreeAuto/TestCase2Login.java
@@ -0,0 +1,30 @@
+
+package Automation.MindtreeAuto;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestCase2Login {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\mindsdet292\\Desktop\\chromedriver.exe");
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://www.urbanladder.com/");
+		
+		driver.findElement(By.xpath("//span[@class='header-icon-link user-profile-icon']")).click();
+		driver.findElement(By.xpath("//a[@class='login-link authentication_popup']")).click();
+		
+		Thread.sleep(5000);
+		driver.findElement(By.xpath("(//input[@id='spree_user_email'])[2]")).click();
+		driver.findElement(By.xpath("(//input[@id='spree_user_email'])[2]")).sendKeys("anushavoona193@gmail.com");
+		driver.findElement(By.xpath("(//input[@id='spree_user_password'])[3]")).click();
+		driver.findElement(By.xpath("(//input[@id='spree_user_password'])[3]")).sendKeys("Anusha@123");
+		driver.findElement(By.xpath("//input[@id='ul_site_login']")).click();
+		
+		Thread.sleep(2000);
+		driver.close();
+	}
+
+}
